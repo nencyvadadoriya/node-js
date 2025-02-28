3d6423cea58ba96bbd5f637b9df5456bbbf489c7
nency <vadadoriyanency8@gmail.com>
2025-02-28 22:12:22 +0530

    node-intro

---
+++ lec-1 intro-node/lec-1.js
@@ -0,0 +1,31 @@
+//History of node js Advanteges of node js
+//=>Node.js was created by Ryan Dahl in 2009 as a way to build scalable, high-performance web applications using JavaScript outside the browser. 
+
+//==> Key Milestones in Node.js History:=>
+
+// 2009: Ryan Dahl released Node.js with V8 as its engine.
+// 2010: NPM (Node Package Manager) was introduced, simplifying package management.
+// 2011: Express.js, a popular web framework for Node.js, was released.
+// 2012: Companies like LinkedIn and Walmart started adopting Node.js.
+// 2015: io.js (a fork of Node.js) merged back into Node.js.
+// 2018: Node.js 10 LTS introduced better security and performance improvements.
+// 2021-Present: Node.js continues to evolve with improved performance, security, and async handling.
+
+//==> Use Cases of Node.js
+
+// ✔ Real-time chat applications
+// ✔ Streaming services (Netflix, YouTube)
+// ✔ API development
+// ✔ IoT applications
+// ✔ Serverless computing
+
+//==>Advantages of Node.js
+
+//1 .Asynchronous & Non-blocking I/O
+//2.Single Programming Language (JavaScript)
+//3.High Performance
+//4.Event-Driven Architecture
+//5.Rich Ecosystem with NPM
+//6.Cross-Platform Support
+//7.Scalability
+//8.Community Support
