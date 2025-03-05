d0831e2adedaabc3634873d6e47bc6e8f6399251
Nency Vadadoriya <vadadoriyanency8@gmail.com>
2025-03-06 01:22:06 +0530

    project-2

--- lec-8 Using Express make Crud Operations/index.js
+++ lec-8 Using Express make Crud Operations/index.js
@@ -1,47 +1,22 @@
 const express = require('express');
-
 const app = express();
-const port = 8000;
-
-let studData = [
-    {
-        name: "Harsh",
-        age: 20,
-        email: "harsh@gmail.com",
-        phone: "235690550",
-        password: "Harsh@124"
-    },
-    {
-        name: "Jeck",
-        age: 18,
-        email: "jeck@gmail.com",
-        phone: "896531756",
-        password: "Jec#421"
-    },
-    {
-        name: "Mohan",
-        age: 25,
-        email: "mohan@gmail.com",
-        phone: "963214796",
-        password: "mohan908@#"
-    }
-];
-
+const port = 9000;
+let studData = [];
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
 
+// Route Table Page
 app.get("/", (req, res) => {
-    let name = "nency vadadoriya";
-    res.render("table", { name, studData })
+    res.render("table", { studData })
 })
 
+// Route Insert Form Page
 app.get('/insert', (req, res) => {
     res.render("form");
 })
-
+// Logic Insert Student in Array
 app.post("/addStud", (req, res) => {
     console.log("Request Data : ", req.body);
-
     const obj = {
         name: req.body.name,
         age: req.body.age,
@@ -49,10 +24,46 @@ app.post("/addStud", (req, res) => {
         phone: req.body.phone,
         password: req.body.password
     }
-
     studData.push(obj);
+    res.redirect("/");
+})
 
-    res.redirect("/insert");
+//  Update in Array
+app.get("/edit", (req, res) => {
+    const id = req.query.id; //1 
+    const data = studData.find((val, i) => id == i); // 1 == 1
+    data.id = req.query.id;
+    console.log(data);
+    res.render("edit", { data });  // 6 
+})
+
+app.post("/updateStudent", (req, res) => {
+    const { id, name, age, email, phone, password } = req.body;
+    console.log("Update Data");
+    console.log(req.body);
+    studData = studData.map((val, i) => {
+        if (i == id) {
+            val.name = name;
+            val.age = age;
+            val.email = email;
+            val.phone = phone;
+            val.password = password;
+        }
+        return val;
+    })
+    res.redirect("/");
+})
+
+//  Delete in Array 
+app.get("/delete", (req, res) => {
+    console.log(req.query.id); //1
+
+    studData = studData.filter((val, index) => index != req.query.id);
+    //                                          2 != 2
+    res.redirect("/");
+})
+app.listen(port, () => {
+    console.log("Server is Started !! ");
 })
 // app.get("/about", (req, res) => {
 //     res.render("about");
@@ -65,7 +76,3 @@ app.post("/addStud", (req, res) => {
 // V => Views => UI => .ejs
 // C => Controllers
 
-
-app.listen(port, () => {
-    console.log("Server is Started !!");
-})
