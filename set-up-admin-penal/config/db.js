42519d489af7117360e751940a1c7b159cc27b4f
Nency Vadadoriya <vadadoriyanency8@gmail.com>
2025-05-05 17:52:03 +0530

    project-6-admin-penal

--- set-up-admin-penal/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const mongoUrl = ('mongodb://localhost:27017/admin');
-const db=mongoose.connection;
-mongoose.connect(mongoUrl);
-db.on('connected', () => console.log('DataBase is Connected..'));
-db.on('error', (err) => console.log('DB is  not Connected..', err));
-db.on('disconnected', () => console.log('DB is Disconnected..'));
-module.exports = db;
